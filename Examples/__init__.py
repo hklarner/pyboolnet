08fbf67c93a4dcfda61df096e32a34cc53a5819e
HannesK <hannes.klarner@fu-berlin.de>
2016-06-28 11:29:27 +0200

    added: get_primes(..) and get_bnet(..)

--- Examples/__init__.py
+++ Examples/__init__.py
@@ -1,3 +1,24 @@
 
+import os
+BASE = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+BASE = os.path.normpath(BASE)
 
+import FileExchange
 import generator
+
+
+
+def get_primes(FnameBNET):
+    assert("." not in FnameBNET)
+    path = os.path.join(BASE,FnameBNET,FnameBNET+".bnet")
+
+    return FileExchange.bnet2primes(path)
+
+
+def get_bnet(FnameBNET):
+    assert("." not in FnameBNET)
+    path = os.path.join(BASE,FnameBNET,FnameBNET+".bnet")
+    with open(path) as f:
+        bnet = f.read()
+
+    return bnet
