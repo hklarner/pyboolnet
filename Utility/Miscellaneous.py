b313d24d5a35b4eb3ada1aa7447b2f7725244cd0
HannesK <hannes.klarner@fu-berlin.de>
2016-06-30 16:04:06 +0200

    added function divide_list_into_similar_length_lists(..)

--- Utility/Miscellaneous.py
+++ Utility/Miscellaneous.py
@@ -1,5 +1,6 @@
 
 
+
 try:
     # Python 2.x
     import ConfigParser as configparser
@@ -9,3 +10,36 @@ except ImportError:
     import configparser
 
 myconfigparser = configparser
+
+
+
+import math
+
+# auxillary to create graph labels that are as square as possible
+# used by e.g. stg2sccgraph / stg2condensationgraph / basin diagram
+
+def divide_list_into_similar_length_lists(List):
+    """
+    used for drawing pretty labels.
+    """
+    
+    width = sum(len(x) for x in List)
+    width = math.sqrt(width)
+
+    stack = list(List)
+    lists = []
+    remaining = sum(map(len,stack))
+    while remaining>width:
+        new  = stack.pop()
+        size = len(new)
+        line = [new]
+        while size<width:
+            new = stack.pop()
+            size+=len(new)
+            line+=[new]
+        lists.append(line)
+        remaining = sum(map(len,stack))
+    if stack:
+        lists.append(stack)
+
+    return lists
