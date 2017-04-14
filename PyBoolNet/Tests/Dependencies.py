e97f46b97d38755f1f8a2e702c728637c389c0ea
HannesK <hannes.klarner@fu-berlin.de>
2017-04-14 15:05:33 +0200

    added tests for espresso / eqntott

--- PyBoolNet/Tests/Dependencies.py
+++ PyBoolNet/Tests/Dependencies.py
@@ -158,6 +158,34 @@ class TestImageMagick(unittest.TestCase):
         self.assertTrue("ImageMagick" in out, msg)
 
 
+class TestEspresso(unittest.TestCase):
+    def test_espresso_responds(self):
+        cmd = config.get("Executables", "espresso")
+        cmd = os.path.join(BASE, "Dependencies", cmd)
+        cmd = os.path.normpath(cmd)
+        cmd = [cmd, "--help"]
+        
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+
+        msg = out
+        msg+= '\nespresso did not respond with "Espresso Version"'
+        msg+= '\ncommand: "%s"'%' '.join(cmd)
+        self.assertTrue("Espresso Version" in out, msg)
+
+    def test_eqntott_responds(self):
+        cmd = config.get("Executables", "eqntott")
+        cmd = os.path.join(BASE, "Dependencies", cmd)
+        cmd = os.path.normpath(cmd)
+        cmd = [cmd, "--help"]
+        
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+
+        msg = '\neqntott did not respond with "usage:"'
+        msg+= '\ncommand: "%s"'%' '.join(cmd)
+        self.assertTrue("usage:" in err, msg)
+
 
 
 
