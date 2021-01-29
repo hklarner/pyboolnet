31fd29568f4feceeb34e54988c5de8a42a802fbd
hklarner <leevilux@yahoo.de>
2021-01-29 21:52:56 +0100

    adds balanced_tree to generators.py

--- PyBoolNet/generators.py
+++ PyBoolNet/generators.py
@@ -1,22 +1,25 @@
 
 
-def line_graph(n: int, edge_sign: int = 1, loop_sign: int = 1) -> dict:
+import networkx
+
+
+def path_graph(n: int, edge_sign: int = 1, loop_sign: int = 1) -> dict:
     """
-    Creates a line graph with `n` nodes.
+    Creates a path graph network with `n` nodes.
     All edges are either positive or negative, depending on `"edge_sign"`.
-    The self-loop of the first component is either positive or negative, depending on `"loop_sign"`.
+    The self-loop of the root component is either positive or negative, depending on `"loop_sign"`.
 
     **arguments**:
-        * *n* (int): prime implicants
+        * *n* (int): number of components
         * *edge_sign* (int): either `1` or `-1` for positive or negative edges
-        * *loop_sign* (int): either `1` or `-1` for positive or negative self-loop of first component
+        * *loop_sign* (int): either `1` or `-1` for positive or negative self-loop of root component
 
     **returns**:
-        * *primes* (dict): primes implicants of line graph
+        * *primes* (dict): primes implicants
 
     **example**::
 
-        >>> primes = line_graph(n=3, edge_sign=-1, loop_sign=1)
+        >>> primes = path_graph(n=3, edge_sign=-1, loop_sign=1)
         >>> ig = primes2igraph(primes)
         >>> print(sorted(ig.edges))
         [('v0', 'v0', {'sign': {1}}), ('v0', 'v1', {'sign': {-1}}), ('v1', 'v2', {'sign': {-1}})]
@@ -29,8 +32,51 @@ def line_graph(n: int, edge_sign: int = 1, loop_sign: int = 1) -> dict:
     primes = {}
     k = 1 if edge_sign == 1 else 0
 
-    for i in range(n - 1):
-        j = i + 1
+    path = networkx.path_graph(n=n, create_using=networkx.DiGraph)
+
+    for i, j in path.edges:
+        primes[f"v{j}"] = [[{f"v{i}": 1-k}], [{f"v{i}": k}]]
+
+    k = 1 if loop_sign == 1 else 0
+    primes[f"v0"] = [[{f"v0": 1 - k}], [{f"v0": k}]]
+
+    return primes
+
+
+def balanced_tree(height: int, branching_factor: int = 2, edge_sign: int = 1, loop_sign: int = 1) -> dict:
+    """
+    Creates a balanced tree network of given `height` and `branching_factor`.
+    All edges are either positive or negative, depending on `"edge_sign"`.
+    The self-loop of the root component is either positive or negative, depending on `"loop_sign"`.
+
+    **arguments**:
+        * *height* (int): height of tree
+        * *branching_factor* (int): branching factor of tree
+        * *edge_sign* (int): either `1` or `-1` for positive or negative edges
+        * *loop_sign* (int): either `1` or `-1` for positive or negative self-loop of first component
+
+    **returns**:
+        * *primes* (dict): primes implicants
+
+    **example**::
+
+        >>> primes = balanced_tree(height=3)
+        >>> ig = primes2igraph(primes)
+        >>> print(sorted(ig.edges))
+        [('v0', 'v0', {'sign': {1}}), ('v0', 'v1', {'sign': {-1}}), ('v1', 'v2', {'sign': {-1}})]
+    """
+
+    assert height >= 1
+    assert branching_factor >= 1
+    assert edge_sign in [1, -1]
+    assert loop_sign in [1, -1]
+
+    tree = networkx.balanced_tree(r=branching_factor, h=height, create_using=networkx.DiGraph)
+
+    primes = {}
+    k = 1 if edge_sign == 1 else 0
+
+    for i, j in tree.edges:
         primes[f"v{j}"] = [[{f"v{i}": 1-k}], [{f"v{i}": k}]]
 
     k = 1 if loop_sign == 1 else 0
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,25 +1,48 @@
 
 
 import pytest
+import networkx
 
 from PyBoolNet.InteractionGraphs import primes2igraph
-from PyBoolNet.generators import line_graph
+from PyBoolNet.generators import path_graph, balanced_tree
 
 
-@pytest.mark.parametrize("n", [3])
-@pytest.mark.parametrize("edge_sign", [1, -1])
-@pytest.mark.parametrize("loop_sign", [1, -1])
-def test_line_graph(n: int, edge_sign: int, loop_sign: int):
-
-    primes = line_graph(n=n, edge_sign=edge_sign, loop_sign=loop_sign)
-    ig = primes2igraph(primes)
-    x = sorted(ig.edges(data=True))
-
-    assert len(primes) == n
-
+def assert_edge_signs_agree(ig: networkx.DiGraph, edge_sign: int, loop_sign: int):
     for u, v, d in ig.edges(data=True):
 
         if u == v:
             assert d["sign"] == {loop_sign, }
         else:
             assert d["sign"] == {edge_sign, }
+
+
+@pytest.mark.parametrize("n", [3])
+@pytest.mark.parametrize("edge_sign", [1, -1])
+@pytest.mark.parametrize("loop_sign", [1, -1])
+def test_path_graph(n: int, edge_sign: int, loop_sign: int):
+
+    primes = path_graph(n=n, edge_sign=edge_sign, loop_sign=loop_sign)
+    ig = primes2igraph(primes)
+
+    path = networkx.path_graph(n=n, create_using=networkx.DiGraph)
+    path.add_edge(0, 0)
+
+    assert networkx.is_isomorphic(ig, path)
+    assert_edge_signs_agree(ig, edge_sign, loop_sign)
+
+
+@pytest.mark.parametrize("height", [3])
+@pytest.mark.parametrize("branching_factor", [2])
+@pytest.mark.parametrize("edge_sign", [1, -1])
+@pytest.mark.parametrize("loop_sign", [1, -1])
+def test_balanced_tree(height: int, branching_factor: int, edge_sign: int, loop_sign: int):
+
+    primes = balanced_tree(height=height, branching_factor=branching_factor, edge_sign=edge_sign, loop_sign=loop_sign)
+    ig = primes2igraph(primes)
+
+    tree = networkx.balanced_tree(r=branching_factor, h=height, create_using=networkx.DiGraph)
+    tree.add_edge(0, 0)
+
+    assert networkx.is_isomorphic(ig, tree)
+    assert_edge_signs_agree(ig, edge_sign, loop_sign)
+
